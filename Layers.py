f0e3f59da02c740ded042912fe0b5cee9a7b192e
emigu777 <emily.guillou1@gmail.com>
2022-11-10 14:57:44 +0100

    Added multilayer

--- Layers.py
+++ Layers.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-class layer:
+class Layer:
 
     def __init__(self, n_units, input_units):       #constructor
         self.n_units = int(n_units)                 #number of units in layer
@@ -11,23 +11,26 @@ class layer:
         self.preactivation = np.empty(self.n_units) #empty array for preactivation values
         self.activation = np.empty(self.n_units)    #empty array for activation values
 
-    def forward_step(self):
+    def forward_step(self, input):
+        self.input = input 
         self.preactivation = np.dot(self.input, self.weights)  #multiply weights matrix with input array and safe results to preactivation array
         for i in range(self.n_units):
             self.preactivation[i] += self.bias[i]              #add biases to each preactivation to get final preactivation
             self.activation[i] = np.maximum(0, self.preactivation[i])   #ReLu (preactivation) = activation
         return self.activation
 
-    #def backward_step(self):
+    def backward_step(self, target, error-signal):
+
+        self.weight_gradient= np.atleast_2d(self.input).T.dot(np.multiply())
 
 
 
 
-l1 = layer(4, 5)
+
+
+l1 = Layer(4, 5)
 print(l1.bias)
 print(l1.weights)
-l1.input = (1, 1, 0, 0, 0)
-print(l1.input)
-l1.forward_step()
+l1.forward_step([1, 1, 0, 0, 0])
 print(l1.preactivation)
 print(l1.activation)
---
+++ Multilayer.py
@@ -0,0 +1,35 @@
+import numpy as np 
+import Layers
+
+class Multilayerperceptron:
+
+    def __init__(self, input, n_layers, n_units ):
+        """
+        Constructs a multilayerperceptron 
+
+            Args: 
+            input(array): the perceptrons input
+            n_layers(int): number of layers
+            n_units(array): number of units per layer
+        """
+        self.n_layers = n_layers                                            
+        self.input = input
+        self.multilayer = np.empty(n_layers)
+        self.multilayer[0] = Layers.Layer(n_units[0], len(self.input))
+        for i in range(1, n_layers):
+            self.multilayer[i] = Layers.Layer(n_units[i], n_units[i-1])
+
+    def forward_step(self):
+        """
+        Propagates the input through the layers
+        """
+        self.output = self.multilayer[0].forward_step(self.input)
+        for i in range(self.n_layers):
+            self.output = self.multilayer[i].forward_step(self.output)
+        return self.output 
+
+    def backpropagation(self):
+        """
+        Updates the weights and biases of the network
+        """
+
