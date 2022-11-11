8a72c98c06765bc3231c2bb1f5c169fadd2c90e0
Saaas1 <sscheerhorn@uni-osnabrueck.de>
2022-11-11 12:58:43 +0100

    backward step
    t.transpose doesnt't work

--- Layers.py
+++ Layers.py
@@ -11,8 +11,10 @@ class layer:
         self.preactivation = np.empty(self.n_units) #empty array for preactivation values
         self.activation = np.empty(self.n_units)    #empty array for activation values
 
-        self.a1 = np.empty(self.n_units)            #a1 array to safe σ(preactivation) ◦dL/dactivation
-        self.dLbydW = np.empty(size = (self.input_units, self.n_units)) #dLbydW matrix for the gradients of the weights
+        self.error_signal_n = np.empty(self.input_units)  #array to safe dLbyda(n) the signal which the layer gets from the layer n+1
+        self.error_signal_for_n_minus_one = np.empty(self.input_units) #array to safe dLbyda(n-1) the signal will be given to layer n-1
+        self.dLbydd = np.empty(self.n_units)            #array to safe dLbydd(n)
+        self.dLbydW = np.empty((self.input_units, self.n_units)) #dLbydW(n) matrix for the gradients of the weights
         self.lastlayer = False                      #our layer is not the last one unless we say self.lastlayer = True
 
     def forward_step(self, input):
@@ -23,27 +25,50 @@ class layer:
             self.activation[i] = np.maximum(0, self.preactivation[i])   #ReLu (preactivation) = activation
         return self.activation
 
-    def backward_step(self, target, ):
-        #we call "σ(preactivation) ◦dL/dactivation" "a1" and calculate it:
-        for i in range(self.n_units):
-            if self.preactivation[i] <= 0:      #ReLu'(preactivation) becomes zero for smaller than zero and one if else
-                self.a1[i] = 0
-            else:
-                self.a1[i] = self.activation[i] - target[i] #activation - target is dL/dactivation, the derivative of the loss function with regard to the activation
-        #return self.a1
-        #calculate dL/dW
-        for i in range(self.input_units)
+    def backward_step(self, error_signal):          #the error signal is the gradient dL/da which the layer gets from the layer n+1
+        self.error_signal = error_signal
+        if self.lastlayer == True:                  #in the last layer the error signal still needs to be calculated with the target, the error signal given when calling the method is the target
+            for i in range(self.n_units):
+                if self.preactivation[i] <= 0:      #ReLu'(preactivation) becomes zero for smaller than zero and one if else
+                    self.dLbydd[i] = 0
+                else:
+                    self.dLbydd[i] = self.activation[i] - self.error_signal[i] #activation - target is dL/dactivation, the derivative of the loss function with regard to the activation. All this times one (because of ReLu') becomes dL/dd
+        else:                                       #the case for every layer but the last one
+            for i in range(self.n_units):
+                if self.preactivation[i] <= 0:
+                    self.dLbydd[i] = 0              #ReLu' becomes zero when the preactivation is <= 0
+                else:
+                    self.dLbydd[i] = self.error_signal[i]
 
+        #now dd(n)/da(n-1) = weights is multiplied with dL/dd(n) to get dL/da(n-1) which is the new error signal given to layer n-1
+        self.error_signal_for_n_minus_one = np.dot(self.weights, self.dLbydd)
+
+        #dL/dW(n) is calculated by dL/dd * input
+        #self.input = np.asarray(self.input)
+        print('dLbydd: ')
+        print(self.dLbydd)
+        print(self.input)
+        self.dLbydW = np.dot(self.input, self.dLbydd)
+
+
+    
 
 
 
 
 l1 = layer(4, 5)
-print(l1.bias)
-print(l1.weights)
+#print(l1.bias)
+#print(l1.weights)
 l1.forward_step([1, 1, 0, 0, 0])
 print(l1.input)
-print(l1.preactivation)
-print(l1.activation)
-l1.backward_step([1, 1, 1, 1, 2])
-print(l1.a1)
+
+
+#print(l1.preactivation)
+#print(l1.activation)
+#l1.lastlayer = True
+l1.backward_step([1, 1, 1, 2])
+print(l1.error_signal)
+print(l1.dLbydd)
+print(l1.error_signal_for_n_minus_one)
+print('dL/dW:')
+print(self.dLbydW)
